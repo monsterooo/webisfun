2ba82ee2db012e947f0527659cc715c6f4bd9b0e
monsterooo <createthink.net@gmail.com>
2025-12-11 15:45:33 +0800

    sandpack use code template

--- components/mdx/sandpack.tsx
+++ components/mdx/sandpack.tsx
@@ -1,8 +1,28 @@
+import { useMemo } from "react";
+import * as codeFiles from "@/content/codes";
 import {
   Sandpack as SandpackAlias,
   SandpackInternal,
 } from "@codesandbox/sandpack-react";
 
-export function Sandpack(props: SandpackInternal) {
-  return <SandpackAlias {...props} />;
+interface SandpackProps extends SandpackInternal {
+  codeId?: string;
+}
+
+export function Sandpack({ codeId, ...props }: SandpackProps) {
+  const codeFile = useMemo(() => {
+    if (!codeId) return null;
+    return codeFiles?.[codeId as keyof typeof codeFiles];
+  }, [codeId]);
+
+  const overrides = useMemo(() => {
+    if (!codeFile) return {};
+    return {
+      files: {
+        ...codeFile,
+      },
+    };
+  }, [codeFile]);
+
+  return <SandpackAlias {...props} {...overrides} />;
 }
