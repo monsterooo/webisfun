5c4823934e3805deb8c8190de89acc8cfe9ffcc9
monsterooo <createthink.net@gmail.com>
2025-12-15 16:14:57 +0800

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+import { allBlogs } from "@/.content-collections/generated";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const blogs = allBlogs.filter((blog) => blog.published);
+  return [
+    {
+      url: "https://webisfun.dev/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://webisfun.dev/writing",
+      lastModified: new Date(),
+    },
+    ...blogs.map((blog) => ({
+      url: `https://webisfun.dev/writing/${blog.slug}`,
+      lastModified: new Date(blog.publishedAt),
+    })),
+  ];
+}
