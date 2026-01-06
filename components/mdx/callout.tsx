559f4f88eb310aafe5c8f9ac3e620b9bc7d620c9
jiangzhu <createthink.net@gmail.com>
2026-01-06 23:02:46 +0800

    callout done

--- components/mdx/callout.tsx
+++ components/mdx/callout.tsx
@@ -1,23 +1,21 @@
 import { cva } from "class-variance-authority";
-import type { VariantProps } from "class-variance-authority";
-import { Info } from "lucide-react";
+import { CircleX, Info, Lightbulb, TriangleAlert } from "lucide-react";
 import { cn } from "@/lib/utils";
 
 interface CalloutProps {
   children: React.ReactNode;
   className?: string;
   title?: string;
-  type?: "info" | "warn" | "error" | "success" | "idea";
+  type?: "info" | "warn" | "error" | "idea";
 }
 
-const calloutVariants = cva("p-4 [&>p]:my-0", {
+const calloutVariants = cva("p-4 [&>p]:my-1 [&>pre]:my-2 rounded-sm", {
   variants: {
     type: {
-      info: "bg-callout-background border-l-4 border-primary",
-      warn: "bg-yellow-50 text-yellow-800 border-yellow-200",
-      error: "bg-red-50 text-red-800 border-red-200",
-      success: "bg-green-50 text-green-800 border-green-200",
-      idea: "bg-gray-50 text-gray-800 border-gray-200",
+      info: "bg-callout-info-background border-l-4 border-primary",
+      warn: "bg-callout-warn-background border-l-4 border-yellow-500",
+      error: "bg-callout-error-background border-l-4 border-red-500",
+      idea: "bg-callout-idea-background border-l-4 border-orange-500",
     },
   },
   defaultVariants: {
@@ -25,14 +23,13 @@ const calloutVariants = cva("p-4 [&>p]:my-0", {
   },
 });
 
-const calloutIconVariants = cva("", {
+const calloutIconVariants = cva("size-6", {
   variants: {
     type: {
-      info: "fill-primary text-callout-background",
-      warn: "text-yellow-800",
-      error: "text-red-800",
-      success: "text-green-800",
-      idea: "text-gray-800",
+      info: "fill-primary text-callout-info-background",
+      warn: "fill-yellow-500 text-callout-warn-background",
+      error: "fill-red-500 text-callout-error-background",
+      idea: "fill-orange-500 text-orange-500",
     },
   },
   defaultVariants: {
@@ -41,12 +38,27 @@ const calloutIconVariants = cva("", {
 });
 
 export function Callout(props: CalloutProps) {
-  const { children, className, type, title, ...rest } = props;
+  const { children, className, type = "info", title, ...rest } = props;
+
+  const getIconComponent = () => {
+    switch (type) {
+      case "info":
+        return <Info className={cn(calloutIconVariants({ type }))} />;
+      case "warn":
+        return <TriangleAlert className={cn(calloutIconVariants({ type }))} />;
+      case "error":
+        return <CircleX className={cn(calloutIconVariants({ type }))} />;
+      case "idea":
+        return <Lightbulb className={cn(calloutIconVariants({ type }))} />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className={cn(calloutVariants({ type }), className)} {...rest}>
-      <div className="flex items-center gap-2">
-        <Info className={cn(calloutIconVariants({ type }))} />
+      <div className="flex items-center gap-2 mb-2">
+        {getIconComponent()}
         <p className="!my-0 font-medium">{title}</p>
       </div>
       {children}
