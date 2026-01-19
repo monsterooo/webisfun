beb4186c218bd94483b79894ebf2478d959ed967
jiangzhu <createthink.net@gmail.com>
2026-01-19 22:09:54 +0800

    hidden the header logo

--- components/header.tsx
+++ components/header.tsx
@@ -5,9 +5,10 @@ export function Header() {
   return (
     <header className="max-w-5xl mx-auto flex justify-between h-12">
       <Link href="/" className="flex justify-center items-center gap-2">
-        <div className="size-6 bg-primary text-white rounded-full text-sm flex justify-center items-center">
+        {/* <div className="size-6 bg-primary text-white rounded-full text-sm flex justify-center items-center">
           W
-        </div>
+        </div> */}
+        {/* <Icons.logo className="size-8 text-whtie" /> */}
         <span className="text-2xl leading-none font-plantin mt-1.5">
           WebIsFun
         </span>
