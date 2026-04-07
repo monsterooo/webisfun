01d772e0ded6d3d0e665b8a6686840ad8824b849
monsterooo <createthink.net@gmail.com>
2026-04-07 16:39:39 +0800

    移除theme库

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
-import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
@@ -46,11 +45,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${plantin.variable} antialiased font-geist`}
       >
-        <ThemeProvider attribute="class">
-          <Header />
-          {children}
-          <Footer />
-        </ThemeProvider>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
